eb1c64317a0c04744da1b29f20443948a1791709
Mhlangano <mhlanganok@yahoo.com>
2019-11-03 07:53:20 +0200

    Enter key, search DB, populate front end

--- DIVE/force-app/main/default/lwc/diveDashboard/diveDashboard.js
+++ DIVE/force-app/main/default/lwc/diveDashboard/diveDashboard.js
@@ -1,25 +1,49 @@
-import { LightningElement, track } from 'lwc';
-import findAccounts from '@salesforce/apex/dive_Populate_Controller.findAccounts';
-const DELAY = 350;
-export default class dive_Dashboard extends LightningElement {
-    @track accounts;
-    @track error;
+import {LightningElement, api, track} from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent'; // import toast message event .
+
+// import apex class and it's methods. 
+import getDataFromObject from '@salesforce/apex/dive_Populate_Controller.getDataFromObject'
+
+export default class dive_Populate_Controller extends LightningElement {
+    @track searchKey;
+    @track data;
+    @track error; // to show error message from apex controller.
+    @track success; // to show succes message in ui.
+    @track InputValues = {
+        strName: "test",
+        strAddress: "test",
+        strCountry: "test",
+        strCode: "1",
+        strNumber: "1"
+      };
+    
     handleKeyChange(event) {
-        // Debouncing this method: Do not actually invoke the Apex call as long as this function is
-        // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
-        window.clearTimeout(this.delayTimeout);
-        const searchKey = event.target.value;
-        // eslint-disable-next-line @lwc/lwc/no-async-operation
-        this.delayTimeout = setTimeout(() => {
-            findAccounts({ searchKey })
-                .then(result => {
-                    this.accounts = result;
-                    this.error = undefined;
-                })
-                .catch(error => {
-                    this.error = error;
-                    this.accounts = undefined;
-                });
-        }, DELAY);
-    }
+        this.searchKey = event.target.value;
+    }    
+    handleKeyPress(event) {
+
+        alert('Number: '+JSON.stringify(this.InputValues)); 
+    }       
+    // method for get  accounts.
+    populateDashboard() { 
+        if(!this.searchKey){
+            return;
+        } 
+        getDataFromObject({ itemID: this.searchKey }) 
+            .then(result => { 
+
+                //let full_obj = JSON.stringify(result);
+   
+                
+                let test = result[0].Profile_JSON__c;
+                let profile_data = JSON.parse(test);
+                alert(JSON.stringify(profile_data));
+                alert(JSON.stringify(profile_data.cust_name)); 
+                this.data = profile_data.cust_name; 
+                 
+            })    
+            .catch(error => {
+                this.error = error;
+            });
+    }  
 }
--- DIVE/force-app/main/default/lwc/diveProfile/diveProfile.js
+++ DIVE/force-app/main/default/lwc/diveProfile/diveProfile.js
@@ -1,4 +1,4 @@
 import { LightningElement, api } from 'lwc';
 export default class dive_Profile extends LightningElement {
-    @api account;
-}  
+    @api profileData ; 
+}   
